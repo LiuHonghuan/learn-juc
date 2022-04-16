a30cdef568acc1dd92108e69652373bc939da0a8
honghuan.Liu <honghuan0921@gmail.com>
2022-04-16 22:43:05 +0800

    2

---
+++ src/main/java/cc/honghuan/jucdemo/JucDemoApplication.java
@@ -0,0 +1,13 @@
+package cc.honghuan.jucdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JucDemoApplication.class, args);
+    }
+
+}
