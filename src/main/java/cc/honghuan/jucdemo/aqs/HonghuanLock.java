215406a8d2210235f5d5a9c567ffcf85b52d35d3
honghuan.Liu <honghuan0921@gmail.com>
2022-04-16 23:47:44 +0800

    2

---
+++ src/main/java/cc/honghuan/jucdemo/aqs/HonghuanLock.java
@@ -0,0 +1,37 @@
+package cc.honghuan.jucdemo.aqs;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author honghuan.Liu
+ * @date 2022/4/16 23:30
+ */
+public class HonghuanLock {
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return  compareAndSetState(0, 1);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState()==1;
+        }
+    }
+
+    private Sync sync = new Sync();
+
+    public void lock () {
+        sync.acquire(1);
+    }
+    public void unlock () {
+        sync.release(1);
+    }
+}
---
+++ src/main/java/cc/honghuan/jucdemo/aqs/Test.java
@@ -0,0 +1,35 @@
+package cc.honghuan.jucdemo.aqs;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author honghuan.Liu
+ * @date 2022/4/16 23:33
+ */
+public class Test {
+
+    static AtomicInteger count = new AtomicInteger(0);
+    static HonghuanLock lock = new HonghuanLock();
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 2000; i++) {
+
+            new Thread(() -> {
+
+                for (int k = 0; k < 10; k++) {
+                    lock.lock();
+                    count.incrementAndGet();
+                    lock.unlock();
+                }
+
+
+            }).start();
+
+
+        }
+
+        System.out.println(count);
+
+    }
+}
