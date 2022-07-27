ea31796aae923263623516ab865ad71c7803a91c
honghuan.Liu <honghuan0921@gmail.com>
2022-07-27 16:45:19 +0800

    2

---
+++ src/main/java/cc/honghuan/jucdemo/netty/NIOClient.java
@@ -0,0 +1,28 @@
+package cc.honghuan.jucdemo.netty;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author honghuan.Liu
+ * @date 2022/7/27 16:31
+ */
+public class NIOClient {
+
+    public static void main(String[] args) throws Exception {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 6666);
+        if (!socketChannel.connect(inetSocketAddress)) {
+            while (!socketChannel.finishConnect()) {
+                System.out.println("连接中。。。。");
+            }
+        }
+        String str = "Hello, NIO";
+        ByteBuffer byteBuffer = ByteBuffer.wrap(str.getBytes());
+        socketChannel.write(byteBuffer);
+
+    }
+}
---
+++ src/main/java/cc/honghuan/jucdemo/netty/NIOServer.java
@@ -0,0 +1,46 @@
+package cc.honghuan.jucdemo.netty;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author honghuan.Liu
+ * @date 2022/7/27 16:23
+ */
+public class NIOServer {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        Selector selector = Selector.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(6666));
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (true) {
+            if (selector.select(1000) == 0) {
+                System.out.println("等待1s, 无连接");
+                continue;
+            }
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> selectionKeyIterator = selectionKeys.iterator();
+            while (selectionKeyIterator.hasNext()) {
+                SelectionKey key = selectionKeyIterator.next();
+                if (key.isAcceptable()) {
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                }
+                if (key.isReadable()) {
+                    SocketChannel channel = (SocketChannel)key.channel();
+                    ByteBuffer buffer = (ByteBuffer)key.attachment();
+                    channel.read(buffer);
+                    System.out.println("收到客户端: "+new String(buffer.array()));
+                }
+                selectionKeyIterator.remove();
+            }
+        }
+    }
+}
